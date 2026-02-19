4df95d5f3a59fb1bf97e9e21a2a30d1790529694
yezidtozzali <yeezusbroo@gmail.com>
2026-02-19 01:26:35 +0100

    add dynamic book cards with read/delete buttons

--- script.js
+++ script.js
@@ -40,14 +40,46 @@ function addBookToLibrary(title, author, page, read){
 
 function displayBook(){
     container.innerHTML = "";
+
+    if(myLibrary.length === 0){
+        const noBook = document.createElement("p");
+        noBook.textContent = "add some books to your library"
+        noBook.classList.add("no-book");
+        container.appendChild(noBook);
+    }
+
+    
+
     myLibrary.forEach(book =>{
     const newDiv = document.createElement("div");
-    newDiv.classList.add("book-display");
-    newDiv.textContent = `${book.title} by ${book.author} - ${book.page} pages`;
+    newDiv.classList.add("book-card");
+
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-display");
+
+    const bookInfo = document.createElement("div");
+    bookInfo.classList.add("book-info");
+
+    const titleH4 = document.createElement("h4");
+    titleH4.classList.add("title-info");
+    titleH4.textContent = book.title;
+
+    const authorInfo = document.createElement("p");
+    authorInfo.classList.add("author-info");
+    authorInfo.textContent = book.author;
+
+    const pageInfo = document.createElement("p");
+    pageInfo.classList.add("page-info");
+    pageInfo.textContent = `${book.page} pages`;
+
 
     const buttonDelete = document.createElement("button");
+    buttonDelete.classList.add("delete-button");
     buttonDelete.textContent = "Delete";
     buttonDelete.dataset.id = book.bookId;
+
+    const buttons = document.createElement("div");
+    buttons.classList.add("buttons");
 
     buttonDelete.addEventListener("click", (e) =>{
         const bookId = e.target.dataset.id;
@@ -63,6 +95,8 @@ function displayBook(){
     const buttonRead = document.createElement("button");
     buttonRead.textContent = book.read ? "Read" : "Not Read";
     buttonRead.dataset.id = book.bookId;
+    buttonRead.classList.add("status-button");
+    buttonRead.classList.add(book.read ? "read" : "not-read")
 
     buttonRead.addEventListener("click", (e) => {
         const bookId = e.target.dataset.id;
@@ -76,9 +110,17 @@ function displayBook(){
 
 
     container.appendChild(newDiv);
-    newDiv.appendChild(buttonDelete);
-    newDiv.appendChild(buttonRead);
+    newDiv.appendChild(bookCard);
+    bookCard.appendChild(bookInfo);
+    bookInfo.appendChild(titleH4);
+    bookInfo.appendChild(authorInfo);
+    bookInfo.appendChild(pageInfo);
+    newDiv.appendChild(buttons);
+    buttons.appendChild(buttonDelete);
+    buttons.appendChild(buttonRead);
     });
+
+    
 };
 
 
@@ -98,6 +140,7 @@ form.addEventListener("submit", (e) =>{
 
     addBookToLibrary(title, author, page, read);
     displayBook();
+    container.lastChild.classList.add("new-book");
     dialog.close();
     form.reset();
 });
@@ -108,4 +151,4 @@ closeButton.addEventListener("click", () =>{
 });
 
 
-
+displayBook();
